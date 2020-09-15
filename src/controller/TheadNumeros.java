abffc9fc447f7855a14d99adfd842c0e925ec423
Junior <Junior@DESKTOP-FBQMUOU>
2020-09-15 17:50:25 -0300

    corrigir botao jogar

--- src/controller/TheadNumeros.java
+++ src/controller/TheadNumeros.java
@@ -6,27 +6,28 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 
 public class TheadNumeros extends Thread{
-    private JTextField tela;
-    private JButton botao;
+	private JTextField tela;
+	private JButton botao;
 	Random Numero = new Random();
-   
+
 	public TheadNumeros(JTextField tela, JButton botaoJogar) {
 		this.tela = tela;
 		this.botao = botaoJogar;
 	}
 
-    
-    public void run() {
-    	botao.setEnabled(false);	
-    	int maximo  = Numero.nextInt(50)+1;
-    	for (int i = 1; i <= maximo; i++) {
-    		tela.setText(Integer.toString(Numero.nextInt(7)+1));
-         try {
-			Thread.sleep(75);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+
+	public void run() {
+		botao.setEnabled(false);	
+		int maximo  = Numero.nextInt(50)+1;
+		for (int i = 1; i <= maximo; i++) {
+			botao.setEnabled(false);	
+			tela.setText(Integer.toString(Numero.nextInt(7)+1));
+			try {
+				Thread.sleep(75);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-    	}
-    	botao.setEnabled(true);	
-    }
+		botao.setEnabled(true);	
+	}
 }
